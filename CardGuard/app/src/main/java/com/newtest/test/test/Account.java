7c62cbada0a18631efde290e9c6fd48a10c91f1b
Nathan <nshowalter832@gmail.com>
2018-02-26 16:44:28 -0500

    started SendReceivepage

--- CardGuard/app/src/main/java/com/newtest/test/test/Account.java
+++ CardGuard/app/src/main/java/com/newtest/test/test/Account.java
@@ -8,6 +8,6 @@ public class Account extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.Account);
+        setContentView(R.layout.account);
     }
 }
--- CardGuard/app/src/main/java/com/newtest/test/test/SendReceivePage.java
+++ CardGuard/app/src/main/java/com/newtest/test/test/SendReceivePage.java
@@ -3,11 +3,11 @@ package com.newtest.test.test;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SendReceivePage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.signin);
+        setContentView(R.layout.sendreceivepage);
     }
 }
