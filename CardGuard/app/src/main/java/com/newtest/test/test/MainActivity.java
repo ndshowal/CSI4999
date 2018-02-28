da07fa9a7850eb4e8bc97326e085cd193d15f65f
ampolito <apolito53@gmail.com>
2018-02-28 01:00:10 -0500

    Fixed app file name issues

--- CardGuard/app/src/main/java/com/newtest/test/test/MainActivity.java
+++ CardGuard/app/src/main/java/com/newtest/test/test/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.signin);
+        setContentView(R.layout.sign_in);
     }
 }
--- CardGuard/app/src/main/java/com/newtest/test/test/ReceivingPage.java
+++ CardGuard/app/src/main/java/com/newtest/test/test/ReceivingPage.java
@@ -8,6 +8,6 @@ public class ReceivingPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.receivingpage);
+        setContentView(R.layout.receiving_page);
     }
 }
