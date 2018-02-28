5e90ad9c328cec2151d030d5cff096f3a9c40d3d
Johnnyvezai <Jli2095x@gmail.com>
2018-02-28 04:01:16 -0500

    Update MainActivity.java

--- CardGuard/app/src/main/java/com/newtest/test/test/MainActivity.java
+++ CardGuard/app/src/main/java/com/newtest/test/test/MainActivity.java
@@ -9,5 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in);
+        
+    }
+    public void buttonOnClick(View v) {
+        Button button=(Button) v;
+        ((Button) v).setText("clicked");
     }
 }
