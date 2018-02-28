98d5e5c604f5f7a57b4025176f115e904288ee55
ndshowal <nshowalter832@gmail.com>
2018-02-28 12:54:13 -0500

    click feature

--- CardGuard/app/src/main/java/com/newtest/test/test/MainActivity.java
+++ CardGuard/app/src/main/java/com/newtest/test/test/MainActivity.java
@@ -1,7 +1,10 @@
 package com.newtest.test.test;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in);
+        Button btn = (Button)findViewById(R.id.button2);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Account.class));
+            }
+        });
         
     }
 }
