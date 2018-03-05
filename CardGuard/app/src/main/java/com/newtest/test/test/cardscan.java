de052313022e22f50ed6dbb57fc0f0ba827d3123
Zehuuuuuuua <zzhao@oakland.edu>
2018-03-05 15:16:30 -0500

    card scan

---
+++ CardGuard/app/src/main/java/com/newtest/test/test/cardscan.java
@@ -0,0 +1,87 @@
+package com.newtest.test.test;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import io.card.payment.CardIOActivity;
+import io.card.payment.CreditCard;
+
+
+public class cardscan extends AppCompatActivity {
+    private static final int REQUEST_SCAN = 101;
+    private static final int REQUEST_AUTOTEST = 200;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cardscan);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button btnscan = (Button)findViewById(R.id.btnScan);
+        btnscan.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(cardscan.this, CardIOActivity.class);
+                intent.putExtra(CardIOActivity.EXTRA_REQUIRE_EXPIRY, true);
+                intent.putExtra(CardIOActivity.EXTRA_SCAN_EXPIRY, true);
+                intent.putExtra(CardIOActivity.EXTRA_REQUIRE_CVV, true);
+                intent.putExtra(CardIOActivity.EXTRA_REQUIRE_CARDHOLDER_NAME, true);
+                intent.putExtra(CardIOActivity.EXTRA_LANGUAGE_OR_LOCALE, "en");
+                intent.putExtra(CardIOActivity.EXTRA_GUIDE_COLOR, Color.GREEN);
+                intent.putExtra(CardIOActivity.EXTRA_RETURN_CARD_IMAGE, true);
+                startActivityForResult(intent, REQUEST_SCAN);
+
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if ((requestCode == REQUEST_SCAN || requestCode == REQUEST_AUTOTEST) && data != null && data.hasExtra(CardIOActivity.EXTRA_SCAN_RESULT))
+        {
+            ((TextView) findViewById(R.id.tvCardDetail)).setVisibility(View.VISIBLE);
+            String resultDisplayStr;
+            if (data != null && data.hasExtra(CardIOActivity.EXTRA_SCAN_RESULT))
+            {
+                CreditCard scanResult = data.getParcelableExtra(CardIOActivity.EXTRA_SCAN_RESULT);
+// Never log a raw card number. Avoid displaying it, but if necessary use getFormattedCardNumber()
+                resultDisplayStr = "Card Number: " + scanResult.getRedactedCardNumber() + "\n";
+// Do something with the raw number, e.g.:
+// myService.setCardNumber( scanResult.cardNumber );
+                if (scanResult.isExpiryValid())
+                {
+                    resultDisplayStr += "Expiration Date: " + scanResult.expiryMonth + "/" + scanResult.expiryYear + "\n";
+                }
+                if (scanResult.cvv != null)
+                {
+// Never log or display a CVV
+                    resultDisplayStr += "CVV has " + scanResult.cvv.length() + " digits.\n";
+                }
+                if (scanResult.postalCode != null)
+                {
+                    resultDisplayStr += "Postal Code: " + scanResult.postalCode + "\n";
+                }
+            }
+            else
+            {
+                resultDisplayStr = "Scan was canceled.";
+            }
+            ((TextView) findViewById(R.id.tvCardDetail)).setText(resultDisplayStr);
+        }
+    }
+}
+
+
