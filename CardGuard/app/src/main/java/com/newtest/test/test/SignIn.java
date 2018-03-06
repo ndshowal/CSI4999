ce0c3e898705c1192ec96e05a06cacd1ee279901
ndshowal <nshowalter832@gmail.com>
2018-03-06 10:59:16 -0500

    added some click events

--- CardGuard/app/src/main/java/com/newtest/test/test/SignIn.java
+++ CardGuard/app/src/main/java/com/newtest/test/test/SignIn.java
@@ -1,7 +1,10 @@
 package com.newtest.test.test;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignIn extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class SignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signin);
+
+        Button btn = (Button)findViewById(R.id.button2);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignIn.this, Account.class));
+            }
+        });
+
+        Button btn2 = (Button)findViewById(R.id.button);
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignIn.this, Register.class));
+            }
+        });
     }
 }
