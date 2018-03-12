a7520fb49e1481f21ebe568a48b2568bc942bc0e
ampolito <apolito53@gmail.com>
2018-03-12 12:57:59 -0400

    Added User.java and Transaction.java to the app

---
+++ CardGuard/app/src/main/java/com/newtest/test/test/Transaction.java
@@ -0,0 +1,133 @@
+package com.newtest.test.test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Transaction {
+    
+    private String transactionID;
+    private Date transactionStartDate;
+    private Date transactionCompleteDate;
+    private User initiator;
+    private User sender;
+    private User recipient;
+    private double transactionAmount;
+    private String memo;
+    private Boolean isProcessed;
+    private Boolean isVerified;
+    
+    // CONSTRUCTOR //
+    public Transaction(Date transactionStartDate, User initiator,
+            User sender, User recipent, double transactionAmount, String memo, 
+            Boolean isProcessed, Boolean isVerified) {
+        this.transactionStartDate = transactionStartDate;
+        this.initiator = initiator;
+        this.sender = sender;
+        this.recipient = recipient;
+        this.transactionAmount = transactionAmount;
+        this.memo = memo;
+        this.isProcessed = isProcessed;
+        this.isVerified = isVerified;
+        createTransactionID();
+    }
+    
+    @Override
+    public String toString() {
+        return "Transaction ID: " + getTransactionID()
+                + " Transaction Initiated:" + getTransactionStartDateString() 
+                + " Transaction Completed:" + getTransactionCompleteDateString() 
+                + " Initiated By: " + initiator.getUsername()
+                + " Sender: " + sender.getUsername()
+                + " Recipient: " + recipient.getUsername() 
+                + " Transaction Amount: " + getTransactionAmount()
+                + " Memo: " + getMemo()
+                + " Processed: " + isProcessed()
+                + " Verified: " + isVerified();
+    }
+    
+    // Creates a hash of the Transaction Initiation Date, the usernames of the
+    //  initiator of the transaction, the sender, and the recipient, and the
+    //  transaction amount.
+    
+    public void createTransactionID() {
+        String toBeHashed = getTransactionStartDate().toString()
+                + initiator.getUsername() 
+                + getSender().getUsername()
+                + getRecipient().getUsername() 
+                + String.valueOf(getTransactionAmount());
+        int transactionID = toBeHashed.hashCode();
+    }
+    
+    
+    // GETTERS //
+    public String getTransactionID() {
+        return transactionID;
+    }
+    
+    public Date getTransactionStartDate() {
+        return transactionStartDate;
+    }
+    
+    public String getTransactionStartDateString() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String transactionStartDateString = sdf.format(transactionStartDate);
+        
+        return transactionStartDateString;
+    }
+    
+    public Date getTransactionCompleteDate() {
+        if(transactionCompleteDate != null) {
+            return transactionCompleteDate;
+        } else {
+            return null;
+        }
+    }
+    
+    public String getTransactionCompleteDateString() {
+        if(getTransactionCompleteDate() != null) {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); 
+            String transactionCompleteDateString = sdf.format(transactionCompleteDate);
+        
+            return transactionCompleteDateString;
+        } else {
+            return "Transaction not yet completed.";
+        }
+    }
+    
+    public User getInitiator() {
+        return initiator;
+    }
+    
+    public User getSender() {
+        return sender;
+    }
+    
+    public User getRecipient() {
+        return recipient;
+    }
+    
+    public double getTransactionAmount() {
+        return transactionAmount;
+    }
+    
+    public String getMemo() {
+        return memo;
+    }
+    
+    public Boolean isProcessed() {
+        return isProcessed;
+    }
+    
+    public Boolean isVerified() {
+        return isVerified;
+    }
+    
+    // SETTERS //
+    public void setTransactionStartDate(Date in) {
+        transactionStartDate = in;
+    }
+    
+    public void setTransactionCompleteDate(Date in) {
+        transactionCompleteDate = in;
+    }
+}
---
+++ CardGuard/app/src/main/java/com/newtest/test/test/User.java
@@ -0,0 +1,81 @@
+package com.newtest.test.test;
+
+import java.util.ArrayList;
+
+public class User {
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private String accountType;
+    private ArrayList<Transaction> transactions;
+    
+    // CONSTRUCTOR //
+    public User(String username, String firstName, String lastName,
+            String emailAddress, String accountType) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+        this.accountType = accountType;
+    }
+    
+    @Override
+    public String toString() {
+        return "Username: " + getUsername() 
+                + " First Name: " + getFirstName() 
+                + " Last Name: " + getLastName() 
+                + " Email Address" + getEmailAddress();
+    }
+    
+    // GETTERS //
+    public String getUsername() {
+        return username;
+    }
+    
+    public String getFirstName() {
+        return firstName;
+    }
+    
+    public String getLastName() {
+        return lastName;
+    }
+    
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+    
+    public String getAccountType() {
+        return accountType;
+    }
+    
+    public ArrayList<Transaction> getTransactions() {
+        return transactions;
+    }
+    
+    // SETTERS //
+    public void setUsername(String in) {
+        username = in;
+    }
+    
+    public void setFirstName(String in) {
+        firstName = in;
+    }
+    
+    public void setLastName(String in) {
+        lastName = in;
+    }
+    
+    public void setEmailAddress(String in) {
+        emailAddress = in;
+    }
+    
+    public void setAccountType(String in) {
+        accountType = in;
+    }
+    
+    // Adds a transaction to the arraylist of transactions for this user
+    public void addTransaction(Transaction tx) {
+        transactions.add(tx);
+    }
+}
