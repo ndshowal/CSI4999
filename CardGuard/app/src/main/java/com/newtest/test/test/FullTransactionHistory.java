bc6698d6340f8e9eef6aa826448bf3d3c2a2bf19
ndshowal <nshowalter832@gmail.com>
2018-03-14 12:58:35 -0400

    .

---
+++ CardGuard/app/src/main/java/com/newtest/test/test/FullTransactionHistory.java
@@ -0,0 +1,13 @@
+package com.newtest.test.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FullTransactionHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.full_transaction_history);
+    }
+}
